e2bbc6bfa7cda4094fad3d47a4c12b138d7bf583
transcendence12 <gosiek.krawczuk@gmail.com>
2024-06-04 09:26:43 +0200

    feat: implement tamagotchi states

--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -1,5 +1,7 @@
 export default class Tamagotchi {
   constructor() {
+    this.state = "playing";
+
     this.health = { value: 10, importance: 1 };
     this.hunger = { value: 10, importance: 3 };
     this.energy = { value: 10, importance: 2 };
@@ -29,11 +31,123 @@ export default class Tamagotchi {
     displayElement.innerText = this.fun.value;
   };
 
+  setState(state){
+    this.state = state;
+  };
+
+  displaySpriteElement = (elementSelector) => {
+    const displayElement = document.querySelector(elementSelector);
+    displayElement.className = `tamago tamago-${this.state}`;
+  };
+
+  displayStateDescriptionElement = (elementSelector) => {
+    const displayElement = document.querySelector(elementSelector);
+    displayElement.textContent = `${this.state}`;
+  };
+
+  checkStateChange = () => {
+    this.checkIfHappy();
+    this.checkIfSad();
+    this.checkIfHungry();
+    this.checkIfSleepy();
+    this.checkIfEating();
+    this.checkIfPlaying();
+    this.checkIfSleeping();
+    this.checkIfDead();
+    this.displayStateInUI();
+  };
+
+  // check each state:
+  checkIfHappy = () => {
+    if (
+      this.health.value >= 7 &&
+      this.fun.value >= 7 &&
+      this.hunger.value >= 7 &&
+      this.energy.value >= 7
+    ) {
+      this.setState("happy");
+    }
+  };
+  checkIfSad = () => {
+    if (this.fun.value <= 6) {
+      this.setState("sad");
+    }
+  };
+  checkIfHungry = () => {
+    if (this.hunger.value <= 6) {
+      this.setState("hungry");
+    }
+  };
+  checkIfSleepy = () => {
+    if (this.energy.value <= 6) {
+      this.setState("sleepy");
+    }
+  };
+  checkIfEating = () => {
+    if (this.hunger.value === 5 || this.hunger.value === 4) {
+      this.setState("eating");
+    }
+  };
+
+  checkIfPlaying = () => {
+    if (
+      this.energy.value >= 8 &&
+      this.hunger.value >= 8 &&
+      this.fun.value >= 8 &&
+      this.health.value >= 8
+    ) {
+      this.setState("playing");
+    }
+  };
+
+  checkIfSleeping = () => {
+    if (this.energy.value === 4 || this.energy.value === 5) {
+      this.setState("sleeping");
+    }
+  };
+
+  checkIfDead = () => {
+    if (this.health.value <= 0) {
+      this.setState("dead");
+    }
+  };
+
+  // display UI:
+  displayStateInUI() {
+    switch (this.state) {
+      case "happy":
+      case "sad":
+      case "hungry":
+      case "sleepy":
+      case "eating":
+      case "playing":
+      case "sleeping":
+      case "dead":
+        this.displaySpriteElement("#sprite-image");
+        this.displayStateDescriptionElement("#tamagoStateDescription");
+        break;
+      default:
+        break;
+    }
+  }
+
   decreaseLifeParams = () => {
-    this.decrementIntervals.energy = setInterval(this.decreaseEnergy, 2000);
-    this.decrementIntervals.hunger = setInterval(this.decreaseHunger, 1000);
-    this.decrementIntervals.fun = setInterval(this.decreaseFun, 1000);
-    this.decrementIntervals.health = setInterval(this.decreaseHealth, 1000);
+    this.decrementIntervals.energy = setInterval(
+      this.decreaseEnergy.bind(this),
+      2000
+    );
+    this.decrementIntervals.hunger = setInterval(
+      this.decreaseHunger.bind(this),
+      1000
+    );
+    this.decrementIntervals.fun = setInterval(
+      this.decreaseFun.bind(this),
+      1000
+    );
+    this.decrementIntervals.health = setInterval(
+      this.decreaseHealth.bind(this),
+      1000
+    );
   };
 
   decreaseEnergy = () => {
@@ -43,6 +157,8 @@ export default class Tamagotchi {
         this.energy.value--;
       }
       if (this.energy.value < 0) this.energy.value = 0;
+      this.displayEnergy("#energy-point-element");
+      this.checkStateChange();
     }
   };
 
@@ -50,6 +166,8 @@ export default class Tamagotchi {
     if (this.hunger.value > 0) {
       this.hunger.value--;
       if (this.hunger.value < 0) this.hunger.value = 0;
+      this.displayHunger("#hunger-point-element");
+      this.checkStateChange();
     }
   };
 
@@ -57,6 +175,8 @@ export default class Tamagotchi {
     if (this.fun.value > 0) {
       this.fun.value--;
       if (this.fun.value < 0) this.fun.value = 0;
+      this.displayFun("#fun-point-element");
+      this.checkStateChange();
     }
   };
 
@@ -66,6 +186,8 @@ export default class Tamagotchi {
         this.health.value--;
       }
       if (this.health.value < 0) this.health.value = 0;
+      this.displayHealth("#health-point-element");
+      this.checkStateChange();
     }
   };
 
@@ -89,5 +211,6 @@ export default class Tamagotchi {
     this.displayEnergy(energyElement);
     this.displayFun(funElement);
     this.decreaseLifeParams();
+    this.checkStateChange();
   };
 }
